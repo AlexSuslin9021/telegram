99a005b9c4b8f19dfa3cc62433763a6b1123fd0b
alexsuslin <alexsuslin@inbox.ru>
2023-08-02 14:10:01 +0300

    create component header.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Header} from "./common/header/header";
 
 
 function App() {
     return (
-        <div>
-            test new
-        </div>
+        <>
+            <Header/>
+        </>
     );
 }
 
