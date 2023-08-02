65b13e6647a699e5fa242b2547bc322d6da606dc
alexsuslin <alexsuslin@inbox.ru>
2023-08-02 14:18:50 +0300

    create body

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Header} from "./common/header/header";
+import {Body} from "./common/body";
 
 
 function App() {
     return (
         <>
             <Header/>
+            <Body/>
         </>
     );
 }
