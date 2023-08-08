997e136cff38f3cb402116b79a7f139e96fd2350
alexsuslin <alexsuslin@inbox.ru>
2023-08-08 12:42:54 +0300

    add debounce in search

--- src/country/component/search/search.tsx
+++ src/country/component/search/search.tsx
@@ -1,16 +1,27 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './search.module.css'
 import {useAppDispatch} from "../../../common/store/store";
 import {getSearch} from "../../slice/country";
+import {useDebounce} from "../../../common/hooks/debounce";
 export const Search = () => {
-const dispatch = useAppDispatch()
-    const [value, setValue]= useState('')
-    const onChange=(e:ChangeEvent<HTMLInputElement>)=>{
-     setValue(e.currentTarget.value)
-        dispatch(getSearch(value))
+    const dispatch = useAppDispatch();
+    const [value, setValue] = useState('');
+    const [debouncedSearchValue, setDebouncedSearchValue] = useState('');
+    const debouncedValue = useDebounce(debouncedSearchValue, 500);
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.currentTarget.value;
+        setValue(newValue);
+        setDebouncedSearchValue(newValue);
     }
-    return (<div className={s.container}>
-        <input className={s.input} value={value} onChange={onChange}/>
+
+    useEffect(() => {
+        dispatch(getSearch(debouncedValue));
+    }, [debouncedValue, dispatch]);
+
+    return (
+        <div className={s.container}>
+            <input className={s.input} value={value} onChange={onChange} />
         </div>
     );
 };
