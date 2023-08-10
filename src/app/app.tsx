17f5a86c01f022cf601b5a2e3f5a5d00220f1f95
alexsuslin <alexsuslin@inbox.ru>
2023-08-10 09:32:38 +0300

    add app slice

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import s from './app.module.css'
-import {Country} from "./country/component/country/country";
-import {Search} from "./country/component/search/search";
-import {Filter} from "./country/filter/filter";
+import {Country} from "../country/component/country/country";
+import {Search} from "../country/component/search/search";
+import {Filter} from "../country/filter/filter";
 
 
 
---
+++ src/app/slice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface InitialState {
+    loader: boolean;
+}
+
+const initialState: InitialState = {
+    loader: false,
+};
+
+const appSlice = createSlice({
+    name: "app",
+    initialState: initialState,
+    reducers: {
+        toggleLoader(state, action: { payload: boolean }) {
+            state.loader = action.payload;
+        },
+    },
+});
+
+export const { toggleLoader } = appSlice.actions;
+export default appSlice.reducer;
